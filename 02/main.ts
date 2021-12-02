08c64d6ad5afd0e5541cd79b135da7844f058006
calebmchenry <calebmchenry@gmail.com>
2021-12-02 09:55:37 -0500

    clean day 2 part 2

--- 02/main.ts
+++ 02/main.ts
@@ -1,69 +1,66 @@
-
 function main() {
-  // part1();
-  part2();
-}
-
-// function part1() {
-//   const movements = Deno.readFileSync("input.txt")
-//     .toString()
-//     .split("\n")
-//     .map((l) => l.split(" "))
-//     .map(([d, n]) => [d, parseInt(n)]);
-//   console.log(movements);
-//   const horizontal = movements
-//     .filter(([direction]) => direction === "forward")
-//     .map(([, num]) => num)
-//     .reduce((prev, current) => prev + current);
-//   const depth = movements
-//     .filter(([direction]) => direction !== "forward")
-//     .map(([d, num]) => (d === "down" ? num : -1 * num))
-//     .reduce((prev, current) => prev + current);
-//   console.log(horizontal * depth);
-// }
-
-async function part2() {
-  type Accumulation = {
-    horizontal: number;
-    depth: number;
-    aim: number;
-  }
-
-  type Instruction = {
-    direction: 'down' | 'forward' | 'up';
-    value: number;
-  }
-
-  function toInstruction(str: string): Instruction {
-    console.log(str);
-    const split = str.split(' ');
-    return {
-      // @ts-expect-error
-      direction: split[0],
-      value: parseInt(split[1]),
-    }
-  }
-
   const instructions = (await Deno.readTextFile("./input.txt"))
     .split("\n")
-    .map(toInstruction)
-
-    const initial: Accumulation =  {horizontal: 0, depth: 0, aim: 0}
-    console.log(instructions);
-    const result = instructions.reduce((acc, instruction) => {
-      console.log(acc);
-      switch(instruction.direction) {
-        case 'down':
-          return {...acc, aim: acc.aim + instruction.value}
-        case 'up':
-          return {...acc, aim: acc.aim - instruction.value}
-        case 'forward':
-          return {...acc, horizontal: acc.horizontal + instruction.value, depth: acc.depth + acc.aim * instruction.value }
-        default: 
-          return acc
-      }
-    }, initial)
-    console.log("part2: ", result.depth * result.horizontal)
+    .map(toInstruction);
+  part1(instructions);
+  part2(instructions);
 }
 
+type Accumulation = {
+  horizontal: number;
+  depth: number;
+  aim: number;
+};
+
+type Instruction = {
+  direction: "down" | "forward" | "up";
+  value: number;
+};
+
+async function part1(instructions: Instruction[]) {
+  const initial: Accumulation = { horizontal: 0, depth: 0, aim: 0 };
+  const result = instructions.reduce((acc, instruction) => {
+    switch (instruction.direction) {
+      case "down":
+        return { ...acc, depth: acc.depth + instruction.value };
+      case "up":
+        return { ...acc, depth: acc.depth - instruction.value };
+      case "forward":
+        return { ...acc, horizontal: acc.horizontal + instruction.value };
+      default:
+        return acc;
+    }
+  }, initial);
+  console.log("part1: ", result.depth * result.horizontal);
+}
+
+async function part2(instructions: Instruction[]) {
+  const initial: Accumulation = { horizontal: 0, depth: 0, aim: 0 };
+  const result = instructions.reduce((acc, instruction) => {
+    switch (instruction.direction) {
+      case "down":
+        return { ...acc, aim: acc.aim + instruction.value };
+      case "up":
+        return { ...acc, aim: acc.aim - instruction.value };
+      case "forward":
+        return {
+          ...acc,
+          horizontal: acc.horizontal + instruction.value,
+          depth: acc.depth + acc.aim * instruction.value,
+        };
+      default:
+        return acc;
+    }
+  }, initial);
+  console.log("part2: ", result.depth * result.horizontal);
+}
+
+function toInstruction(str: string): Instruction {
+  const split = str.split(" ");
+  return {
+    // @ts-expect-error
+    direction: split[0],
+    value: parseInt(split[1]),
+  };
+}
 main();
