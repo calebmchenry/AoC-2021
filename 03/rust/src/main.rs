6102bb8e638978b49ac676e312e9dfb2a2352e58
calebmchenry <calebmchenry@gmail.com>
2021-12-06 07:51:20 -0500

    clean up rust solution day3 part1

--- 03/rust/src/main.rs
+++ 03/rust/src/main.rs
@@ -10,10 +10,10 @@ fn part1(binaries: &Vec<&str>) -> u128 {
     gamma(binaries) * epsilon(binaries)
 }
 
-fn gamma(binaries: &Vec<&str>) -> u128 {
+fn most_common_bits(binaries: &Vec<&str>) -> Vec<f64> {
     let length = binaries.len();
     let initial = vec![0; 12];
-    let gamma_as_str = binaries
+    binaries
         .iter()
         .fold(initial, |acc, &current| {
             let s = String::from(current);
@@ -23,37 +23,35 @@ fn gamma(binaries: &Vec<&str>) -> u128 {
                 .collect::<Vec<u32>>()
         })
         .iter()
-        .map(|&sum| ((sum as f64) / (length as f64)).round().to_string())
+        .map(|&sum| {
+            ((sum as f64) / (length as f64)).round()
+        })
+        .collect::<Vec<f64>>()
+}
+
+fn gamma_binary(binaries: &Vec<&str>) -> String {
+    most_common_bits(binaries )
+        .iter()
+        .map(|b| b.to_string())
         .collect::<Vec<String>>()
-        .join("");
-    print!("gamma: {:?}\n", gamma_as_str);
-    let g = u128::from_str_radix(&gamma_as_str, 2).unwrap();
-    print!("gamma: {:?}\n", g);
-    g
+        .join("")
+}
+
+fn gamma(binaries: &Vec<&str>) -> u128 {
+    let gamma_as_str = gamma_binary(binaries);
+    u128::from_str_radix(&gamma_as_str, 2).unwrap()
+}
+
+fn epsilon_binary(binaries: &Vec<&str>) -> String {
+    most_common_bits(binaries)
+        .iter()
+        .map(|&n| if n == 0.0 { 1 } else { 0 })
+        .map(|n| n.to_string())
+        .collect::<Vec<String>>()
+        .join("")
 }
 
 fn epsilon(binaries: &Vec<&str>) -> u128 {
-    let length = binaries.len();
-    let initial = vec![0; 12];
-    let gamma_as_str = binaries
-        .iter()
-        .fold(initial, |acc, &current| {
-            let s = String::from(current);
-            acc.iter()
-                .zip(s.chars().map(|c| c.to_digit(10).unwrap()))
-                .map(|(&x, y)| x + y)
-                .collect::<Vec<u32>>()
-        })
-        .iter()
-        .map(|&sum| ((sum as f64) / (length as f64)).round())
-        .map(|n| {
-            if n == 0.0 { 1 } else { 0 }
-        })
-        .map(|n| n.to_string())
-        .collect::<Vec<String>>()
-        .join("");
-    print!("epsilon: {:?}\n", gamma_as_str);
-    let g = u128::from_str_radix(&gamma_as_str, 2).unwrap();
-    print!("epsilon: {:?}\n", g);
-    g
+    let epsilon_as_str = epsilon_binary(binaries);
+    u128::from_str_radix(&epsilon_as_str, 2).unwrap()
 }
