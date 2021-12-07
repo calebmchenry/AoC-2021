0e28dc383cfa7a66e39447cd594775db2967876e
calebmchenry <calebmchenry@gmail.com>
2021-12-07 07:16:22 -0500

    day 7 part 2 haskell

--- 07/Main.hs
+++ 07/Main.hs
@@ -6,12 +6,23 @@ splitOnComma s = case dropWhile (==',') s of
   s' -> w : splitOnComma s''
     where (w, s'') = break (==',') s'
 
-cost :: [Int] -> Int -> Int
-cost ns p = sum $ map (abs . (p -)) ns
+unitsToFuel :: Int -> Int
+unitsToFuel n = sum [1..n] 
 
+cost1 :: [Int] -> Int -> Int
+cost1 ns p = sum $ map (abs . (p -)) ns
+
+cost2 :: [Int] -> Int -> Int
+cost2 ns p = sum $ map (unitsToFuel . abs . (p -)) ns
 
 part1 :: [Int] -> Int
-part1 ns = foldl min maxBound $ map (cost ns) [minPosition..maxPosition]
+part1 ns = foldl min maxBound $ map (cost1 ns) [minPosition..maxPosition]
+  where
+    maxPosition = foldl max 0 ns
+    minPosition = foldl min maxPosition ns
+
+part2 :: [Int] -> Int
+part2 ns = foldl min maxBound $ map (cost2 ns) [minPosition..maxPosition]
   where
     maxPosition = foldl max 0 ns
     minPosition = foldl min maxPosition ns
@@ -21,4 +32,5 @@ main = do
   contents <- hGetContents handle
   let ns = map read $ splitOnComma contents
   print $ part1 ns
+  print $ part2 ns
   hClose handle
