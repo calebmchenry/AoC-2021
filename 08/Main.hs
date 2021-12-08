82845b96a6b95b75cf43bd96ff8dec1698f5e96b
calebmchenry <calebmchenry@gmail.com>
2021-12-08 09:34:44 -0500

    day 8 part 1 clean up

--- 08/Main.hs
+++ 08/Main.hs
@@ -7,9 +7,10 @@ splitOnPipe s = case dropWhile (=='|') s of
     where (w, s'') = break (=='|') s'
 
 part1 :: String -> Int
-part1 content = length $ filter (\l -> l == 2 || l == 4 || l == 3 || l == 7) $ map length signals
+part1 content = length $ filter (`elem` uniqueLengths) $ map length signals
   where
     signals = (>>= words) $ map (last . splitOnPipe) $ lines content
+    uniqueLengths = [2, 4, 3, 7]
 
 main = do
   handle <- openFile "input.txt" ReadMode
